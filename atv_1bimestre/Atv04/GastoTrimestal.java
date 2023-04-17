0f5813c6e382b9168ccab3033e2aeb87ceafa552
Débora Peres <124798553+debdebinha@users.noreply.github.com>
2023-04-16 22:07:43 -0300

    commit update

--- atv_1bimestre/Atv04/GastoTrimestal.java
+++ atv_1bimestre/Atv04/GastoTrimestal.java
@@ -3,13 +3,13 @@ package atv_1bimestre.Atv04; //declarando o package, "Atv04" dentro da pasta "at
 public class GastoTrimestral{ //criando a classe principal dando inicio ao programa
     public static void main(String [] args){ //criando o metodo principal onde o programa se inicia
 
-        float gastosJan = 30000,00; // Criando e dando o valor da primeira variavel
-        float gastosFev = 33030,77; // Criando e dando o valor da segunda variavel
-        float gastosMar = 23899,01; // Criando e dando o valor da terceira variavel
+        double gastosJaneiro = 30000.00; // Criando e dando o valor da primeira variavel
+        double gastosFevereiro = 33030.77; // Criando e dando o valor da segunda variavel
+        double gastosMarco = 23899.01; // Criando e dando o valor da terceira variavel
 
-        float gastoTotal = gastosJan + gastosFev + gastosMar; //calculo dos gastos do trimestre
+        double gastoTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco; //calculo dos gastos do trimestre
 
-        System.out.println("Total gasto no Trimestre: " + gastoTotal); //Print o valor total gasto no trimestre
+        System.out.println("Total gasto no Trimestre: " + gastoTrimestre); //Print o valor total gasto no trimestre
         //fim do progtrama
     }
 }
