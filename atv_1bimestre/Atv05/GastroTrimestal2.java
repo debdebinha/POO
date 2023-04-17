7f62db6425d269fa2529c8a8de74ea1ca4a4c393
Débora Peres <124798553+debdebinha@users.noreply.github.com>
2023-04-16 22:34:02 -0300

    Update GastroTrimestal2.java

--- atv_1bimestre/Atv05/GastroTrimestal2.java
+++ atv_1bimestre/Atv05/GastroTrimestal2.java
@@ -1,21 +1,21 @@
 package atv_1bimestre.Atv05; //declarando o package, "Atv05" dentro da pasta "atv_1bimestre"
 
 public class GastoTrimestral2{ //criando a classe principal dando inicio ao programa
-    public static void main(String [] args){ //criando o metodo principal onde o programa se inicia
+    public static void main(String [] args){ //criando o metodo principal
 
-        float gastosJan = 30000,00; // Criando e dando o valor da primeira variavel
-        float gastosFev = 33030,77; // Criando e dando o valor da segunda variavel
-        float gastosMar = 23899,01; // Criando e dando o valor da terceira variavel
+        double gastosJaneiro = 30000.00; // Criando e dando o valor da primeira variavel
+        double gastosFevereiro = 33030.77; // Criando e dando o valor da segunda variavel
+        double gastosMarco = 23899.01; // Criando e dando o valor da terceira variavel
 
-        float gastoTotal = gastosJan + gastosFev + gastosMar; //calculo dos gastos do trimestre
+        double gastoTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco; //calculo dos gastos do trimestre
 
-        System.out.println("Total gasto no Trimestre: " + gastoTotal); //Print o valor total gasto no trimestre
+        System.out.println("Total gasto no Trimestre: " + gastoTrimestre); //Print do valor total gasto no trimestre
         
-        calculoMensal(gastoTotal); //chamando o metodo calculomensal
+        calculoMensal(gastoTrimestre); //chamando o calculomensal
     }
-     public static void calculoMensal(int gastoTotal){ //criando um metodo para fazer o calculo do gasto mensal
+     public static void calculoMensal(int gastoTrimestre){ //metodo para realizar o calculo do gasto mensal
 
-        int mediaMensal = (gastoTotal / 3); //calculo simples
+        double mediaMensal = (gastoTrimestre / 3); //calculo do gasto mensal
 
         System.out.println("Valor da média mensal = " + mediaMensal); //Print do gasto mensal
        //fim. 
