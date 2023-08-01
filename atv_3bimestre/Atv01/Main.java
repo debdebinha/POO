6c0b4b7796615ed24dac740c30c43e1d7b488b58
Débora Peres <124798553+debdebinha@users.noreply.github.com>
2023-08-01 00:11:38 -0300

    Create Main.java

---
+++ atv_3bimestre/Atv01/Main.java
@@ -0,0 +1,16 @@
+package animais;
+// Classe principal
+public class Main {
+    // Teste dos metodos implementados
+    public static void main(String[] args) {
+        Cachorro cachorro = new Cachorro("fifi", 4, "Labrador");
+        cachorro.emitirSom();
+        cachorro.mover();
+        cachorro.abanarRabo();
+        Passaros passaros = new Passaros("mia", 2, "cinza");
+        passaros.construirNinho();
+        passaros.emitirSom();
+        passaros.voar();
+
+    }
+}
