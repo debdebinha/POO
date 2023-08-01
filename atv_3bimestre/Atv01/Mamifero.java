b684b17392f9f5afe1734d633521c993b2012b71
Débora Peres <124798553+debdebinha@users.noreply.github.com>
2023-08-01 00:12:14 -0300

    Create Mamifero.java

---
+++ atv_3bimestre/Atv01/Mamifero.java
@@ -0,0 +1,14 @@
+package animais;
+//Classe Mamifero
+public class Mamifero{
+    String nome; //atribulto
+    int idade = 0; //atribulto
+    //metodo emitir som
+    public void emitirSom() {
+        System.out.println("Emitir som...");
+    }
+    //metodo mover
+    public void mover() {
+         System.out.println("Mover...");
+    }   
+}  
