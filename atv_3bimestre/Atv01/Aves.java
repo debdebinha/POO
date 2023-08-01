01f6b98830799c08767541d66f4fd138d25596a4
Débora Peres <124798553+debdebinha@users.noreply.github.com>
2023-08-01 00:10:19 -0300

    Create Aves.java

---
+++ atv_3bimestre/Atv01/Aves.java
@@ -0,0 +1,14 @@
+package animais;
+//Classe Aves
+public class Aves {
+    String nome;
+    int idade;
+// metodo emitir som
+    public void emitirSom() {
+        System.out.println("Emitindo som");
+    }
+//metodo voar
+    public void voar() {
+        System.out.println("Voando");
+    }
+}
